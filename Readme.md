585ecbbeff26ae42225c2dda164361725b897ff3
Ashish Kapoor <swiftobjc@gmail.com>
2021-11-06 15:57:54 +0530

    Add POST GET request

--- Readme.md
+++ Readme.md
@@ -11,3 +11,17 @@
 - Longitude - Number
 - Created At - DateTime
 - Updated At - DateTime
+
+Sample LogEntry JSON
+
+```
+{
+  "title": "Empire state building",
+  "comments": "what a fun being here!",
+  "latitude": 40.748817,
+  "longitude": -78.985428,
+  "rating": 7,
+  "image": "https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg",
+  "visitDate": "2021-11-06T09:52:33.000+00:00"
+}
+```
--- src/index.js
+++ src/index.js
@@ -2,26 +2,41 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config({ path: '.env' });
+
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
+
+app.enable('trust proxy'); // needed for rate limiting by Client IP
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, () => console.log('DB setup success!'));
+
 app.use(morgan('common'));
 app.use(helmet());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  }),
-);
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
 
-app.get('/', (_, res) => {
-  res.json({ message: 'hello world' });
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
 });
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-const port = process.env.PORT || 4000;
-
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Process started on: http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
